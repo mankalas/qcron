2eee6da54476d1e335cebd5501a539a575a9a947
vincent <vincent@groupe-sa.fr>
2015-12-18 10:42:46 +0100

    Empty shell, just to set up CMake.

---
+++ src/qcron.hpp
@@ -0,0 +1,15 @@
+#ifndef _QCRON_HPP
+#define _QCRON_HPP
+
+#include <QObject>
+
+class QCron : public QObject
+{
+    Q_OBJECT
+
+public:
+    QCron();
+    ~QCron();
+};
+
+#endif
---
+++ test/qcron_test.hpp
@@ -0,0 +1,14 @@
+#ifndef _QCRON_TEST_HPP
+#define _QCRON_TEST_HPP
+
+#include <QObject>
+
+class QCronTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void basics();
+};
+
+#endif
