d6dc9b9ef6516d9304759ce4e345a5067aeb8011
Alexey Andreyev <aa13q@ya.ru>
2022-11-01 00:41:47 +0300

    Replace QString::SkipEmptyParts with Qt::SkipEmptyParts
    Since the previous API deprecated since 5.14 and has been removed in Qt6
    
    See also:
    
    https://doc.qt.io/qt-5/qstring-obsolete.html#SplitBehavior-enum

--- src/qcron.cpp
+++ src/qcron.cpp
@@ -99,7 +99,11 @@ _parsePattern(const QString & pattern)
         _setError("'\n' is an invalid field separator.");
         return;
     }
+#if QT_VERSION >= QT_VERSION_CHECK( 5, 14, 0 )
+    QStringList fields = pattern.simplified().split(" ", Qt::SkipEmptyParts);
+#else
     QStringList fields = pattern.simplified().split(" ", QString::SkipEmptyParts);
+#endif
     int nb_fields = fields.size();
     if (nb_fields != 6)
     {
